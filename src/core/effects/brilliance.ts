9b0e2695a86a8ba4d9eb41da56d79c1412603755
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2024-07-19 22:51:08 +0800

    Fix Brilliance period

--- src/core/effects/brilliance.ts
+++ src/core/effects/brilliance.ts
@@ -1,4 +1,4 @@
-import { EventType, createPassiveSource } from '../game';
+import { EventType, createEffectCardSource } from '../game';
 import { lerp } from '../lerp';
 import { log } from '../log';
 import { EventPriority } from '../priorities';
@@ -9,17 +9,23 @@ const MAX_PERIOD = 0.2;
 const MAX_SPEED = 750;
 
 function getBrilliancePeriod(speed: number): number {
-  return lerp(MIN_PERIOD, MAX_PERIOD, Math.max(speed / MAX_SPEED, 1));
+  return lerp(MIN_PERIOD, MAX_PERIOD, Math.min(speed / MAX_SPEED, 1));
 }
 
-export default createPassiveSource({
+export default createEffectCardSource({
   name: 'Brilliance',
   tier: 1,
   getDescription(level) {
     return [
       'Periodically gains ',
-      0.2 * level,
-      ' mana. Base period is 5 seconds.',
+      level,
+      ' mana. Period ranges from',
+      5,
+      ' seconds to ',
+      0.2,
+      ' seconds based on ',
+      'Speed',
+      '.',
     ];
   },
   load(game, player, level) {
@@ -31,7 +37,7 @@ export default createPassiveSource({
       const cleanup = createTick(() => {
         // Calculate period
         elapsed += FRAME_DURATION;
-        if (elapsed >= period) {
+        if (elapsed >= period * 1000) {
           elapsed = 0;
           period = getBrilliancePeriod(player.speedStacks);
 
