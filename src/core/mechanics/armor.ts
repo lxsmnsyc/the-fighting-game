8888ff320fe52dc1e385d9381025b9fb8460eed5
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2024-07-20 23:52:19 +0800

    Update armor.ts

--- src/core/mechanics/armor.ts
+++ src/core/mechanics/armor.ts
@@ -1,4 +1,4 @@
-import { EventType, type Game } from '../game';
+import { EventType, type Game, Stack } from '../game';
 import { log } from '../log';
 import { BuffPriority, DamagePriority, DebuffPriority } from '../priorities';
 
@@ -10,27 +10,26 @@ export function setupArmorMechanics(game: Game): void {
   game.on(EventType.Damage, DamagePriority.Armor, event => {
     if (!event.flags.missed) {
       // Get 50% of the armor
-      const currentArmor = event.target.stacks.armor;
+      const currentArmor = event.target.stacks[Stack.Armor];
       if (currentArmor > 0) {
         const consumable = (currentArmor * CONSUMABLE_ARMOR_STACKS) | 0;
         event.amount -= consumable;
-        game.triggerDebuff(
-          EventType.RemoveArmor,
-          event.source,
-          event.target,
-          consumable,
-        );
+        game.removeStack(Stack.Armor, event.source, event.target, consumable);
       }
     }
   });
 
-  game.on(EventType.AddArmor, BuffPriority.Exact, event => {
-    log(`${event.source.name} gained ${event.amount} stacks of Armor`);
-    event.source.stacks.armor += event.amount;
+  game.on(EventType.AddStack, BuffPriority.Exact, event => {
+    if (event.type === Stack.Armor) {
+      log(`${event.source.name} gained ${event.amount} stacks of Armor`);
+      event.source.stacks[Stack.Armor] += event.amount;
+    }
   });
 
-  game.on(EventType.RemoveArmor, DebuffPriority.Exact, event => {
-    log(`${event.target.name} lost ${event.amount} stacks of Armor`);
-    event.target.stacks.armor -= event.amount;
+  game.on(EventType.RemoveStack, DebuffPriority.Exact, event => {
+    if (event.type === Stack.Armor) {
+      log(`${event.target.name} lost ${event.amount} stacks of Armor`);
+      event.source.stacks[Stack.Armor] -= event.amount;
+    }
   });
 }
