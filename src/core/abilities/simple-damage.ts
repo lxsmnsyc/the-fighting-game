c9b08a6705c6dad15e55ab6b0dd67d48c34af9a7
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2024-07-21 02:07:38 +0800

    Add test ability

---
+++ src/core/abilities/simple-damage.ts
@@ -0,0 +1,25 @@
+import { DamageType, EventType, createAbilityCardSource } from '../game';
+import { EventPriority } from '../priorities';
+
+export default createAbilityCardSource({
+  name: 'Simple Damage',
+  getDescription(level) {
+    return ['Deals ', level * 5, ' Magic damage.'];
+  },
+  load(game, player, level) {
+    game.on(EventType.CastAbility, EventPriority.Post, event => {
+      if (event.source === player) {
+        game.dealDamage(
+          DamageType.Magic,
+          player,
+          game.getOppositePlayer(player),
+          level * 50,
+          {
+            critical: false,
+            missed: false,
+          },
+        );
+      }
+    });
+  },
+});
--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,23 @@
 import { setupGame } from './core';
-import snowball from './core/abilities/snowball';
-import card001 from './core/effects/tier-1/periodic buff/card-002';
-import card002 from './core/effects/tier-1/periodic buff/card-006';
+import simpleDamage from './core/abilities/simple-damage';
+import card106 from './core/effects/add-stack-periodic/card-106';
+import card002 from './core/effects/add-stat-periodic/card-002';
+
 import { Game, Player } from './core/game';
 
 const playerA = new Player('Player A');
 const playerB = new Player('Player B');
 const game = new Game(playerA, playerB);
 
-playerA.AbilityCard = { source: snowball, level: 5 };
-playerB.AbilityCard = { source: snowball, level: 5 };
+playerA.AbilityCard = { source: simpleDamage, level: 5 };
+playerB.AbilityCard = { source: simpleDamage, level: 5 };
 
 playerA.effects = [
-  { source: card001, level: 5 },
+  { source: card106, level: 5 },
   { source: card002, level: 5 },
 ];
 playerB.effects = [
-  { source: card001, level: 5 },
+  { source: card106, level: 5 },
   { source: card002, level: 5 },
 ];
 
