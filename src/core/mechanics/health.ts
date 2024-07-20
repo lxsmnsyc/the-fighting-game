3b588319696b43f524724eb92fb63def6511a783
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2024-07-21 00:36:09 +0800

    Update health.ts

--- src/core/mechanics/health.ts
+++ src/core/mechanics/health.ts
@@ -1,27 +1,94 @@
-import { EventType, type Game } from '../game';
+import { EventType, type Game, Stat } from '../game';
 import { log } from '../log';
-import { BuffPriority, DebuffPriority } from '../priorities';
+import { StatPriority } from '../priorities';
 
 export function setupHealthMechanics(game: Game): void {
   log('Setting up Health mechanics.');
-  game.on(EventType.AddHealth, BuffPriority.Exact, event => {
-    log(`${event.source.name} gained ${event.amount} of Health`);
-    event.source.health = Math.min(
-      event.source.maxHealth,
-      event.source.health + event.amount,
-    );
+  game.on(EventType.SetStat, StatPriority.Exact, event => {
+    if (event.type === Stat.Health) {
+      log(`${event.source.name}'s Health changed to ${event.amount}`);
+      event.source.stats[Stat.Health] = Math.max(0, event.amount);
+    }
   });
 
-  game.on(EventType.RemoveHealth, DebuffPriority.Exact, event => {
-    const deduction = Math.min(event.target.health, event.amount);
-    log(`${event.target.name} lost ${deduction} of Health`);
-    event.target.health -= deduction;
+  game.on(EventType.AddStat, StatPriority.Exact, event => {
+    if (event.type === Stat.Health) {
+      log(`${event.source.name} gained ${event.amount} of Health`);
+
+      game.setStat(
+        Stat.Health,
+        event.source,
+        event.source.stats[Stat.Health] + event.amount,
+      );
+    }
   });
 
-  game.on(EventType.RemoveHealth, DebuffPriority.Post, event => {
-    if (event.target.health === 0) {
-      log(`${event.target.name} died.`);
-      game.endGame(event.source, event.target);
+  game.on(EventType.RemoveStat, StatPriority.Exact, event => {
+    if (event.type === Stat.Health) {
+      log(`${event.target.name} lost ${event.amount} of Health`);
+
+      game.setStat(
+        Stat.Health,
+        event.target,
+        event.target.stats[Stat.Health] - event.amount,
+      );
+    }
+  });
+
+  // Death condition
+  game.on(EventType.SetStat, StatPriority.Post, event => {
+    if (event.type === Stat.Health && event.source.stats[Stat.Health] === 0) {
+      log(`${event.source.name} died.`);
+      game.endGame(game.getOppositePlayer(event.source), event.source);
+    }
+  });
+
+  log('Setting up Max Health mechanics.');
+  game.on(EventType.SetStat, StatPriority.Exact, event => {
+    if (event.type === Stat.MaxHealth) {
+      log(`${event.source.name}'s MaxHealth changed to ${event.amount}`);
+      event.source.stats[Stat.MaxHealth] = Math.max(1, event.amount);
+    }
+  });
+
+  game.on(EventType.AddStat, StatPriority.Exact, event => {
+    if (event.type === Stat.MaxHealth) {
+      log(`${event.source.name} gained ${event.amount} of Max Health`);
+      // Get the current health percentage
+      const currentHealth =
+        event.source.stats[Stat.Health] / event.source.stats[Stat.MaxHealth];
+
+      game.setStat(
+        Stat.MaxHealth,
+        event.source,
+        event.source.stats[Stat.MaxHealth] + event.amount,
+      );
+      // Rescale
+      game.setStat(
+        Stat.Health,
+        event.source,
+        currentHealth * event.source.stats[Stat.MaxHealth],
+      );
+    }
+  });
+
+  game.on(EventType.RemoveStat, StatPriority.Exact, event => {
+    if (event.type === Stat.MaxHealth) {
+      log(`${event.target.name} lost ${event.amount} of Max Health`);
+      // Get the current health percentage
+      const currentHealth =
+        event.source.stats[Stat.Health] / event.source.stats[Stat.MaxHealth];
+      game.setStat(
+        Stat.MaxHealth,
+        event.source,
+        event.source.stats[Stat.MaxHealth] - event.amount,
+      );
+      // Rescale
+      game.setStat(
+        Stat.Health,
+        event.source,
+        currentHealth * event.source.stats[Stat.MaxHealth],
+      );
     }
   });
 }
