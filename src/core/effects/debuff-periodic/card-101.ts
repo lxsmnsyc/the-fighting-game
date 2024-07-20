41928ea87a91983c1da438cc4f6b95a50dce3503
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2024-07-20 13:53:45 +0800

    Move cards

--- src/core/effects/debuff-periodic/card-101.ts
+++ src/core/effects/debuff-periodic/card-101.ts
@@ -1,25 +1,25 @@
-import { EventType, createEffectCardSource } from '../../../game';
-import { lerp } from '../../../lerp';
-import { log } from '../../../log';
-import { EventPriority } from '../../../priorities';
-import { FRAME_DURATION, createTick } from '../../../tick';
+import { EventType, createEffectCardSource } from '../../game';
+import { lerp } from '../../lerp';
+import { log } from '../../log';
+import { EventPriority } from '../../priorities';
+import { FRAME_DURATION, createTick } from '../../tick';
 
 const MIN_PERIOD = 5000;
 const MAX_PERIOD = 200;
 const MAX_SPEED = 750;
 
-function getCard105Period(speed: number): number {
+function getCard101Period(speed: number): number {
   return lerp(MIN_PERIOD, MAX_PERIOD, Math.min(speed / MAX_SPEED, 1));
 }
 
 export default createEffectCardSource({
-  name: 'Card #105',
+  name: 'Card #101',
   tier: 1,
   getDescription(level) {
     return [
       'Periodically applies ',
       level,
-      ' stacks of Slow to the enemy. Period ranges from',
+      ' stacks of Poison to the enemy. Period ranges from',
       5,
       ' seconds to ',
       0.2,
@@ -29,20 +29,20 @@ export default createEffectCardSource({
     ];
   },
   load(game, player, level) {
-    log(`Setting up Card 105 for ${player.name}`);
+    log(`Setting up Card 101 for ${player.name}`);
     game.on(EventType.Start, EventPriority.Post, () => {
       let elapsed = 0;
-      let period = getCard105Period(player.speedStacks);
+      let period = getCard101Period(player.speedStacks);
 
       const cleanup = createTick(() => {
         // Calculate period
         elapsed += FRAME_DURATION;
         if (elapsed >= period) {
           elapsed -= period;
-          period = getCard105Period(player.speedStacks);
+          period = getCard101Period(player.speedStacks);
 
           game.triggerDebuff(
-            EventType.RemoveSpeed,
+            EventType.AddPoison,
             player,
             game.getOppositePlayer(player),
             level,
--- src/core/effects/debuff-periodic/card-102.ts
+++ src/core/effects/debuff-periodic/card-102.ts
@@ -1,25 +1,25 @@
-import { EventType, createEffectCardSource } from '../../../game';
-import { lerp } from '../../../lerp';
-import { log } from '../../../log';
-import { EventPriority } from '../../../priorities';
-import { FRAME_DURATION, createTick } from '../../../tick';
+import { EventType, createEffectCardSource } from '../../game';
+import { lerp } from '../../lerp';
+import { log } from '../../log';
+import { EventPriority } from '../../priorities';
+import { FRAME_DURATION, createTick } from '../../tick';
 
 const MIN_PERIOD = 5000;
 const MAX_PERIOD = 200;
 const MAX_SPEED = 750;
 
-function getCard104Period(speed: number): number {
+function getCard102Period(speed: number): number {
   return lerp(MIN_PERIOD, MAX_PERIOD, Math.min(speed / MAX_SPEED, 1));
 }
 
 export default createEffectCardSource({
-  name: 'Card #104',
+  name: 'Card #102',
   tier: 1,
   getDescription(level) {
     return [
       'Periodically applies ',
       level,
-      ' stacks of Critical Decay to the enemy. Period ranges from',
+      ' stacks of Armor Decay to the enemy. Period ranges from',
       5,
       ' seconds to ',
       0.2,
@@ -29,20 +29,20 @@ export default createEffectCardSource({
     ];
   },
   load(game, player, level) {
-    log(`Setting up Card 104 for ${player.name}`);
+    log(`Setting up Card 102 for ${player.name}`);
     game.on(EventType.Start, EventPriority.Post, () => {
       let elapsed = 0;
-      let period = getCard104Period(player.speedStacks);
+      let period = getCard102Period(player.speedStacks);
 
       const cleanup = createTick(() => {
         // Calculate period
         elapsed += FRAME_DURATION;
         if (elapsed >= period) {
           elapsed -= period;
-          period = getCard104Period(player.speedStacks);
+          period = getCard102Period(player.speedStacks);
 
           game.triggerDebuff(
-            EventType.RemoveCritical,
+            EventType.RemoveArmor,
             player,
             game.getOppositePlayer(player),
             level,
