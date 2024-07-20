b3f072533ec2b55e9ff18a886ba622ea3e2017fb
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2024-07-21 00:19:41 +0800

    Update critical.ts

--- src/core/mechanics/critical.ts
+++ src/core/mechanics/critical.ts
@@ -1,4 +1,4 @@
-import { DamageType, EventType, type Game } from '../game';
+import { DamageType, EventType, type Game, Stack, Stat } from '../game';
 import { lerp } from '../lerp';
 import { log } from '../log';
 import { BuffPriority, DamagePriority, DebuffPriority } from '../priorities';
@@ -23,24 +23,28 @@ export function setupCriticalMechanics(game: Game): void {
       return;
     }
     // Check if player can crit
-    if (event.type === DamageType.Attack && event.source.criticalStacks !== 0) {
+    if (
+      event.type === DamageType.Attack &&
+      event.source.stacks[Stack.Critical] !== 0
+    ) {
       // If there's a critical stack
-      if (event.source.criticalStacks > 0) {
+      if (event.source.stacks[Stack.Critical] > 0) {
         // Calculat critical chance
         const currentCriticalChance = getCriticalChance(
-          event.source.criticalStacks,
+          event.source.stacks[Stack.Critical],
         );
         // Push your luck
         const random = Math.random() * 100;
         if (random <= currentCriticalChance) {
-          event.amount *= event.source.critMultiplier;
+          event.amount *= event.source.stats[Stat.CritMultiplier];
           log(`${event.target.name} triggered a ${event.amount} of critical.`);
           event.flags.critical = true;
 
           const consumable =
-            (event.target.criticalStacks * CONSUMABLE_CRITICAL_STACKS) | 0;
-          game.triggerDebuff(
-            EventType.RemoveCritical,
+            (event.source.stacks[Stack.Critical] * CONSUMABLE_CRITICAL_STACKS) |
+            0;
+          game.removeStack(
+            Stack.Critical,
             event.source,
             event.target,
             consumable,
@@ -48,19 +52,75 @@ export function setupCriticalMechanics(game: Game): void {
         }
       } else {
         const consumable =
-          (event.target.criticalStacks * CONSUMABLE_CRITICAL_STACKS) | 0;
-        game.triggerBuff(EventType.AddCritical, event.source, -consumable);
+          (event.source.stacks[Stack.Critical] * CONSUMABLE_CRITICAL_STACKS) |
+          0;
+        game.addStack(Stack.Critical, event.source, -consumable);
       }
     }
   });
 
-  game.on(EventType.AddCritical, BuffPriority.Exact, event => {
-    log(`${event.source.name} gained ${event.amount} stacks of Critical`);
-    event.source.criticalStacks += event.amount;
+  game.on(EventType.SetStack, BuffPriority.Exact, event => {
+    if (event.type === Stack.Critical) {
+      log(`${event.source.name}'s Critical stacks changed to ${event.amount}`);
+      event.source.stacks[Stack.Critical] = event.amount;
+    }
   });
 
-  game.on(EventType.RemoveCritical, DebuffPriority.Exact, event => {
-    log(`${event.target.name} lost ${event.amount} stacks of Critical`);
-    event.target.criticalStacks -= event.amount;
+  game.on(EventType.AddStack, BuffPriority.Exact, event => {
+    if (event.type === Stack.Critical) {
+      log(`${event.source.name} gained ${event.amount} stacks of Critical`);
+      game.setStack(
+        Stack.Critical,
+        event.source,
+        event.source.stacks[Stack.Critical] + event.amount,
+      );
+    }
+  });
+
+  game.on(EventType.RemoveStack, DebuffPriority.Exact, event => {
+    if (event.type === Stack.Critical) {
+      log(`${event.target.name} lost ${event.amount} stacks of Critical`);
+      game.setStack(
+        Stack.Critical,
+        event.target,
+        event.target.stacks[Stack.Critical] - event.amount,
+      );
+    }
+  });
+
+  log('Setting up Critical Multiplier mechanics.');
+  game.on(EventType.SetStat, BuffPriority.Exact, event => {
+    if (event.type === Stat.CritMultiplier) {
+      log(
+        `${event.source.name}'s Critical Multiplier changed to ${event.amount}`,
+      );
+      event.source.stats[Stat.CritMultiplier] = Math.max(0, event.amount);
+    }
+  });
+
+  game.on(EventType.AddStat, BuffPriority.Exact, event => {
+    if (event.type === Stat.CritMultiplier) {
+      log(
+        `${event.source.name} gained ${event.amount} stacks of Critical Multiplier`,
+      );
+      game.setStat(
+        Stat.CritMultiplier,
+        event.source,
+        event.source.stats[Stat.CritMultiplier] + event.amount,
+      );
+    }
+  });
+
+  game.on(EventType.RemoveStat, DebuffPriority.Exact, event => {
+    if (event.type === Stat.CritMultiplier) {
+      log(
+        `${event.target.name} lost ${event.amount} stacks of Critical Multiplier`,
+      );
+      game.setStat(
+        Stat.CritMultiplier,
+        event.target,
+        event.target.stats[Stat.CritMultiplier] - event.amount,
+      );
+    }
   });
 }
