57422e9bdf8c4a0e69863da85160983ee9677478
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2025-01-03 10:26:08 +0800

    Update tick.ts

--- src/core/mechanics/tick.ts
+++ src/core/mechanics/tick.ts
@@ -1,32 +1,79 @@
-import { type Game, RoundEventType } from '../game';
-import { EventPriority } from '../priorities';
+import type { Game } from '../game';
+import { lerp } from '../lerp';
+import type { Player } from '../player';
+import type { Round } from '../round';
+import { EventPriority, GameEventType, RoundEventType, Stack } from '../types';
 
 const FPS = 60;
 const FPS_DURATION = 1000 / FPS;
 
 export function setupTickMechanics(game: Game): void {
-  game.on(RoundEventType.Start, EventPriority.Post, () => {
-    let elapsed = Date.now();
-    let raf = requestAnimationFrame(update);
+  game.on(GameEventType.NextRound, EventPriority.Pre, ({ round }) => {
+    round.on(RoundEventType.Start, EventPriority.Post, () => {
+      let elapsed = Date.now();
+      let raf = requestAnimationFrame(update);
 
-    function update() {
-      raf = requestAnimationFrame(update);
-      const current = Date.now();
-      let diff = current - elapsed;
-      elapsed = current;
+      function update() {
+        raf = requestAnimationFrame(update);
+        const current = Date.now();
+        let diff = current - elapsed;
+        elapsed = current;
 
-      while (diff >= FPS_DURATION && !game.closed) {
-        game.tick(FPS_DURATION);
-        diff -= FPS_DURATION;
+        while (diff >= FPS_DURATION && !round.closed) {
+          round.tick(FPS_DURATION);
+          diff -= FPS_DURATION;
+        }
+
+        if (diff > 0) {
+          elapsed -= diff;
+        }
       }
 
-      if (diff > 0) {
-        elapsed -= diff;
-      }
-    }
-
-    game.on(RoundEventType.Close, EventPriority.Pre, () => {
-      cancelAnimationFrame(raf);
+      round.on(RoundEventType.End, EventPriority.Pre, () => {
+        cancelAnimationFrame(raf);
+      });
     });
+  });
+}
+
+const MAX_SPEED = 750;
+
+function getPeriod(
+  speed: number,
+  minPeriod: number,
+  maxPeriod: number,
+): number {
+  return lerp(
+    minPeriod * 1000,
+    maxPeriod * 1000,
+    Math.min(speed / MAX_SPEED, 1),
+  );
+}
+
+export interface UsePeriodOptions {
+  round: Round;
+  player: Player;
+  period: { min: number; max: number };
+  run(): void;
+}
+
+export function usePeriod(options: UsePeriodOptions): void {
+  let elapsed = 0;
+  let period = getPeriod(
+    options.player.stats[Stack.Speed],
+    options.period.min,
+    options.period.max,
+  );
+  options.round.on(RoundEventType.Tick, EventPriority.Exact, event => {
+    elapsed += event.delta;
+    if (elapsed >= period) {
+      elapsed -= period;
+      period = getPeriod(
+        options.player.stats[Stack.Speed],
+        options.period.min,
+        options.period.max,
+      );
+      options.run();
+    }
   });
 }
