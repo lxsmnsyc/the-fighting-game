deee41b74b081902acbbef77a4298f20781b1826
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2025-01-03 10:31:22 +0800

    Update damage.ts

--- src/core/mechanics/damage.ts
+++ src/core/mechanics/damage.ts
@@ -1,16 +1,24 @@
-import { DamageFlags } from '../damage-flags';
-import { type Game, RoundEventType, Stat } from '../game';
+import type { Game } from '../game';
 import { log } from '../log';
-import { DamagePriority } from '../priorities';
+import {
+  DamageFlags,
+  DamagePriority,
+  EventPriority,
+  GameEventType,
+  RoundEventType,
+  Stat,
+} from '../types';
 
 export function setupDamageMechanics(game: Game): void {
-  log('Setting up Damage mechanics.');
-  game.on(RoundEventType.Damage, DamagePriority.Exact, event => {
-    if (!(event.flag & DamageFlags.Missed)) {
-      log(
-        `${event.source.name} dealt ${event.amount} damage to ${event.target.name}`,
-      );
-      game.removeStat(Stat.Health, event.target, event.amount);
-    }
+  game.on(GameEventType.NextRound, EventPriority.Pre, ({ round }) => {
+    log('Setting up Damage mechanics.');
+    round.on(RoundEventType.Damage, DamagePriority.Exact, event => {
+      if (!(event.flag & DamageFlags.Missed)) {
+        log(
+          `${event.source.owner.name} dealt ${event.amount} damage to ${event.target.owner.name}`,
+        );
+        round.removeStat(Stat.Health, event.target, event.amount);
+      }
+    });
   });
 }
