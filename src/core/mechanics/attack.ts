61b42cdc206762e3eac6eee1c5441aa5487f2075
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2025-01-03 10:26:12 +0800

    Update attack.ts

--- src/core/mechanics/attack.ts
+++ src/core/mechanics/attack.ts
@@ -1,35 +1,43 @@
-import { type Game, RoundEventType, Stat } from '../game';
+import type { Game } from '../game';
 import { log } from '../log';
-import { StatPriority } from '../priorities';
+import {
+  EventPriority,
+  GameEventType,
+  RoundEventType,
+  Stat,
+  StatPriority,
+} from '../types';
 
 export function setupAttackMechanics(game: Game): void {
-  log('Setting up Attack mechanics.');
-  game.on(RoundEventType.SetStat, StatPriority.Exact, event => {
-    if (event.type === Stat.Attack) {
-      log(`${event.source.name}'s Attack changed to ${event.amount}`);
-      event.source.stats[Stat.Attack] = Math.max(0, event.amount);
-    }
-  });
+  game.on(GameEventType.NextRound, EventPriority.Pre, ({ round }) => {
+    log('Setting up Attack mechanics.');
+    round.on(RoundEventType.SetStat, StatPriority.Exact, event => {
+      if (event.type === Stat.Attack) {
+        log(`${event.source.owner.name}'s Attack changed to ${event.amount}`);
+        event.source.stats[Stat.Attack] = Math.max(0, event.amount);
+      }
+    });
 
-  game.on(RoundEventType.AddStat, StatPriority.Exact, event => {
-    if (event.type === Stat.Attack) {
-      log(`${event.source.name} gained ${event.amount} of Attack`);
-      game.setStat(
-        Stat.Attack,
-        event.source,
-        event.source.stats[Stat.Attack] + event.amount,
-      );
-    }
-  });
+    round.on(RoundEventType.AddStat, StatPriority.Exact, event => {
+      if (event.type === Stat.Attack) {
+        log(`${event.source.owner.name} gained ${event.amount} of Attack`);
+        round.setStat(
+          Stat.Attack,
+          event.source,
+          event.source.stats[Stat.Attack] + event.amount,
+        );
+      }
+    });
 
-  game.on(RoundEventType.RemoveStat, StatPriority.Exact, event => {
-    if (event.type === Stat.Attack) {
-      log(`${event.source.name} lost ${event.amount} of Attack`);
-      game.setStat(
-        Stat.Attack,
-        event.source,
-        event.source.stats[Stat.Attack] - event.amount,
-      );
-    }
+    round.on(RoundEventType.RemoveStat, StatPriority.Exact, event => {
+      if (event.type === Stat.Attack) {
+        log(`${event.source.owner.name} lost ${event.amount} of Attack`);
+        round.setStat(
+          Stat.Attack,
+          event.source,
+          event.source.stats[Stat.Attack] - event.amount,
+        );
+      }
+    });
   });
 }
