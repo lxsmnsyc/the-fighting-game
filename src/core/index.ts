c03ee8874ef1fabdc8e11982eab34f5f23747d7f
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2025-01-04 22:29:52 +0800

    Update index.ts

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,18 +1,5 @@
-import { Game } from './game';
-import { log } from './log';
-import { setupArmorMechanics } from './mechanics/armor';
-import { setupAttackMechanics } from './mechanics/attack';
-import { setupCriticalMechanics } from './mechanics/critical';
-import { setupCureMechanics } from './mechanics/cure';
-import { setupDamageMechanics } from './mechanics/damage';
-import { setupEvasionMechanics } from './mechanics/evasion';
-import { setupHealthMechanics } from './mechanics/health';
-import { setupLuckMechanics } from './mechanics/luck';
-import { setupMagicMechanics } from './mechanics/magic';
-import { setupSpeedMechanics } from './mechanics/speed';
-import { setupTickMechanics } from './mechanics/tick';
-import { EventPriority } from './priorities';
-import { GameEventType } from './types';
+import type { Game } from './game';
+import { EventPriority, GameEventType } from './types';
 
 export function setupGame(game: Game): void {
   game.on(GameEventType.Setup, EventPriority.Exact, () => {
@@ -22,18 +9,34 @@ export function setupGame(game: Game): void {
   });
 
   game.on(GameEventType.Start, EventPriority.Exact, () => {
-    game.openShop();
+    // Display the starter card selection
   });
 
   game.on(GameEventType.OpenShop, EventPriority.Exact, () => {
+    // do stuff
+  });
 
+  game.on(GameEventType.AcquireCard, EventPriority.Exact, () => {
+    // do stuff
+  });
+
+  game.on(GameEventType.SellCard, EventPriority.Exact, () => {
+    // do stuff
+  });
+
+  game.on(GameEventType.EnableCard, EventPriority.Exact, ({ card }) => {
+    card.enabled = true;
+  });
+
+  game.on(GameEventType.EnableCard, EventPriority.Exact, ({ card }) => {
+    card.enabled = false;
   });
 
   game.on(GameEventType.NextRound, EventPriority.Exact, () => {
-
+    // do stuff
   });
 
   game.on(GameEventType.End, EventPriority.Exact, () => {
-
+    // do stuff
   });
 }
