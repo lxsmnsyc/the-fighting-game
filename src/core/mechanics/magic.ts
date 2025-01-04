5c19c89d2aeeef7bb609d7ea2ceea96fa5efdd12
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2025-01-04 09:14:35 +0800

    Update magic.ts

--- src/core/mechanics/magic.ts
+++ src/core/mechanics/magic.ts
@@ -1,35 +1,43 @@
-import { type Game, RoundEventType, Stat } from '../game';
+import type { Game } from '../game';
 import { log } from '../log';
-import { StatPriority } from '../priorities';
+import {
+  EventPriority,
+  GameEventType,
+  RoundEventType,
+  Stat,
+  StatPriority,
+} from '../types';
 
 export function setupMagicMechanics(game: Game): void {
-  log('Setting up Magic mechanics.');
-  game.on(RoundEventType.SetStat, StatPriority.Exact, event => {
-    if (event.type === Stat.Magic) {
-      log(`${event.source.name}'s Magic changed to ${event.amount}`);
-      event.source.stats[Stat.Magic] = Math.max(0, event.amount);
-    }
-  });
+  game.on(GameEventType.NextRound, EventPriority.Pre, ({ round }) => {
+    log('Setting up Magic mechanics.');
+    round.on(RoundEventType.SetStat, StatPriority.Exact, event => {
+      if (event.type === Stat.Magic) {
+        log(`${event.source.owner.name}'s Magic changed to ${event.amount}`);
+        event.source.stats[Stat.Magic] = Math.max(0, event.amount);
+      }
+    });
 
-  game.on(RoundEventType.AddStat, StatPriority.Exact, event => {
-    if (event.type === Stat.Magic) {
-      log(`${event.source.name} gained ${event.amount} of Magic`);
-      game.setStat(
-        Stat.Magic,
-        event.source,
-        event.source.stats[Stat.Magic] + event.amount,
-      );
-    }
-  });
+    round.on(RoundEventType.AddStat, StatPriority.Exact, event => {
+      if (event.type === Stat.Magic) {
+        log(`${event.source.owner.name} gained ${event.amount} of Magic`);
+        round.setStat(
+          Stat.Magic,
+          event.source,
+          event.source.stats[Stat.Magic] + event.amount,
+        );
+      }
+    });
 
-  game.on(RoundEventType.RemoveStat, StatPriority.Exact, event => {
-    if (event.type === Stat.Magic) {
-      log(`${event.source.name} lost ${event.amount} of Magic`);
-      game.setStat(
-        Stat.Magic,
-        event.source,
-        event.source.stats[Stat.Magic] - event.amount,
-      );
-    }
+    round.on(RoundEventType.RemoveStat, StatPriority.Exact, event => {
+      if (event.type === Stat.Magic) {
+        log(`${event.source.owner.name} lost ${event.amount} of Magic`);
+        round.setStat(
+          Stat.Magic,
+          event.source,
+          event.source.stats[Stat.Magic] - event.amount,
+        );
+      }
+    });
   });
 }
