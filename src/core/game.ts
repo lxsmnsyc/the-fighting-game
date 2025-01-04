930dfe1c66ae46c55c47fd8dcd6cbbb42c760388
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2025-01-04 22:29:56 +0800

    Update game.ts

--- src/core/game.ts
+++ src/core/game.ts
@@ -1,4 +1,5 @@
 import { AleaRNG } from './alea';
+import type { CardInstance } from './card';
 import { EventEmitter, type EventEmitterListener } from './event-emitter';
 import { Player } from './player';
 import type { Round } from './round';
@@ -12,12 +13,28 @@ export interface NextRoundGameEvent extends BaseGameEvent {
   round: Round;
 }
 
+export interface CardGameEvent extends BaseGameEvent {
+  card: CardInstance;
+}
+
+export interface CardGameEvent extends BaseGameEvent {
+  card: CardInstance;
+}
+
+export interface CardGameEvent extends BaseGameEvent {
+  card: CardInstance;
+}
+
 export type GameEvents = {
   [GameEventType.Setup]: BaseGameEvent;
   [GameEventType.Start]: BaseGameEvent;
   [GameEventType.End]: BaseGameEvent;
   [GameEventType.NextRound]: NextRoundGameEvent;
   [GameEventType.OpenShop]: BaseGameEvent;
+  [GameEventType.SellCard]: CardGameEvent;
+  [GameEventType.AcquireCard]: CardGameEvent;
+  [GameEventType.EnableCard]: CardGameEvent;
+  [GameEventType.DisableCard]: CardGameEvent;
 };
 
 type GameEventEmitterInstances = {
@@ -31,6 +48,10 @@ function createGameEventEmitterInstances(): GameEventEmitterInstances {
     [GameEventType.End]: new EventEmitter(),
     [GameEventType.NextRound]: new EventEmitter(),
     [GameEventType.OpenShop]: new EventEmitter(),
+    [GameEventType.SellCard]: new EventEmitter(),
+    [GameEventType.AcquireCard]: new EventEmitter(),
+    [GameEventType.EnableCard]: new EventEmitter(),
+    [GameEventType.DisableCard]: new EventEmitter(),
   };
 }
 
@@ -46,15 +67,23 @@ export class Game {
     name: string,
   ) {
     this.rng = new AleaRNG(seed);
-    this.player = new Player(name);
+    this.player = new Player(this.rng.int32(), name);
   }
 
   on<E extends GameEventType>(
     type: E,
     priority: number,
     listener: EventEmitterListener<GameEvents[E]>,
+  ): () => void {
+    return this.emitters[type].on(priority, listener);
+  }
+
+  off<E extends GameEventType>(
+    type: E,
+    priority: number,
+    listener: EventEmitterListener<GameEvents[E]>,
   ): void {
-    this.emitters[type].on(priority, listener);
+    this.emitters[type].off(priority, listener);
   }
 
   emit<E extends GameEventType>(type: E, event: GameEvents[E]): void {
@@ -62,22 +91,38 @@ export class Game {
   }
 
   setup() {
-    this.emit(GameEventType.Setup, { id: 'SetupEvent' });
+    this.emit(GameEventType.Setup, { id: 'Setup' });
   }
 
   start() {
-    this.emit(GameEventType.Start, { id: 'StartEvent' });
+    this.emit(GameEventType.Start, { id: 'Start' });
   }
 
   nextRound(round: Round) {
-    this.emit(GameEventType.NextRound, { id: 'NextRoundEvent', round });
+    this.emit(GameEventType.NextRound, { id: 'NextRound', round });
   }
 
   end() {
-    this.emit(GameEventType.End, { id: 'EndEvent' });
+    this.emit(GameEventType.End, { id: 'End' });
   }
 
   openShop() {
-    this.emit(GameEventType.OpenShop, { id: 'OpenShopEvent' });
+    this.emit(GameEventType.OpenShop, { id: 'OpenShop' });
+  }
+
+  sellCard(card: CardInstance) {
+    this.emit(GameEventType.SellCard, { id: 'SellCard', card });
+  }
+
+  acquireCard(card: CardInstance) {
+    this.emit(GameEventType.AcquireCard, { id: 'AcquireCard', card });
+  }
+
+  enableCard(card: CardInstance) {
+    this.emit(GameEventType.EnableCard, { id: 'EnableCard', card });
+  }
+
+  disableCard(card: CardInstance) {
+    this.emit(GameEventType.DisableCard, { id: 'DisableCard', card });
   }
 }
