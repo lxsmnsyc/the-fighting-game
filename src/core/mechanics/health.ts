c1cace10c8af92e30198b7d2f76c6a79c025f201
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2025-01-04 09:13:24 +0800

    Update health.ts

--- src/core/mechanics/health.ts
+++ src/core/mechanics/health.ts
@@ -1,94 +1,104 @@
-import { type Game, RoundEventType, Stat } from '../game';
+import type { Game } from '../game';
 import { log } from '../log';
-import { StatPriority } from '../priorities';
+import {
+  EventPriority,
+  GameEventType,
+  RoundEventType,
+  Stat,
+  StatPriority,
+} from '../types';
 
 export function setupHealthMechanics(game: Game): void {
-  log('Setting up Health mechanics.');
-  game.on(RoundEventType.SetStat, StatPriority.Exact, event => {
-    if (event.type === Stat.Health) {
-      log(`${event.source.name}'s Health changed to ${event.amount}`);
-      event.source.stats[Stat.Health] = Math.max(0, event.amount);
-    }
-  });
+  game.on(GameEventType.NextRound, EventPriority.Pre, ({ round }) => {
+    log('Setting up Health mechanics.');
+    round.on(RoundEventType.SetStat, StatPriority.Exact, event => {
+      if (event.type === Stat.Health) {
+        log(`${event.source.owner.name}'s Health changed to ${event.amount}`);
+        event.source.stats[Stat.Health] = Math.max(0, event.amount);
+      }
+    });
 
-  game.on(RoundEventType.AddStat, StatPriority.Exact, event => {
-    if (event.type === Stat.Health) {
-      log(`${event.source.name} gained ${event.amount} of Health`);
+    round.on(RoundEventType.AddStat, StatPriority.Exact, event => {
+      if (event.type === Stat.Health) {
+        log(`${event.source.owner.name} gained ${event.amount} of Health`);
 
-      game.setStat(
-        Stat.Health,
-        event.source,
-        event.source.stats[Stat.Health] + event.amount,
-      );
-    }
-  });
+        round.setStat(
+          Stat.Health,
+          event.source,
+          event.source.stats[Stat.Health] + event.amount,
+        );
+      }
+    });
 
-  game.on(RoundEventType.RemoveStat, StatPriority.Exact, event => {
-    if (event.type === Stat.Health) {
-      log(`${event.source.name} lost ${event.amount} of Health`);
+    round.on(RoundEventType.RemoveStat, StatPriority.Exact, event => {
+      if (event.type === Stat.Health) {
+        log(`${event.source.owner.name} lost ${event.amount} of Health`);
 
-      game.setStat(
-        Stat.Health,
-        event.source,
-        event.source.stats[Stat.Health] - event.amount,
-      );
-    }
-  });
+        round.setStat(
+          Stat.Health,
+          event.source,
+          event.source.stats[Stat.Health] - event.amount,
+        );
+      }
+    });
 
-  // Death condition
-  game.on(RoundEventType.SetStat, StatPriority.Post, event => {
-    if (event.type === Stat.Health && event.source.stats[Stat.Health] === 0) {
-      log(`${event.source.name} died.`);
-      game.endGame(game.getOppositePlayer(event.source), event.source);
-    }
-  });
+    // Death condition
+    round.on(RoundEventType.SetStat, StatPriority.Post, event => {
+      if (event.type === Stat.Health && event.source.stats[Stat.Health] === 0) {
+        log(`${event.source.owner.name} died.`);
+        round.end(round.getEnemyUnit(event.source), event.source);
+      }
+    });
 
-  log('Setting up Max Health mechanics.');
-  game.on(RoundEventType.SetStat, StatPriority.Exact, event => {
-    if (event.type === Stat.MaxHealth) {
-      log(`${event.source.name}'s MaxHealth changed to ${event.amount}`);
-      event.source.stats[Stat.MaxHealth] = Math.max(1, event.amount);
-    }
-  });
+    log('Setting up Max Health mechanics.');
+    round.on(RoundEventType.SetStat, StatPriority.Exact, event => {
+      if (event.type === Stat.MaxHealth) {
+        log(
+          `${event.source.owner.name}'s MaxHealth changed to ${event.amount}`,
+        );
+        event.source.stats[Stat.MaxHealth] = Math.max(1, event.amount);
+      }
+    });
 
-  game.on(RoundEventType.AddStat, StatPriority.Exact, event => {
-    if (event.type === Stat.MaxHealth) {
-      log(`${event.source.name} gained ${event.amount} of Max Health`);
-      // Get the current health percentage
-      const currentHealth =
-        event.source.stats[Stat.Health] / event.source.stats[Stat.MaxHealth];
+    round.on(RoundEventType.AddStat, StatPriority.Exact, event => {
+      if (event.type === Stat.MaxHealth) {
+        log(`${event.source.owner.name} gained ${event.amount} of Max Health`);
+        // Get the current health percentage
+        const currentHealth =
+          event.source.stats[Stat.Health] / event.source.stats[Stat.MaxHealth];
 
-      game.setStat(
-        Stat.MaxHealth,
-        event.source,
-        event.source.stats[Stat.MaxHealth] + event.amount,
-      );
-      // Rescale
-      game.setStat(
-        Stat.Health,
-        event.source,
-        currentHealth * event.source.stats[Stat.MaxHealth],
-      );
-    }
-  });
+        round.setStat(
+          Stat.MaxHealth,
+          event.source,
+          event.source.stats[Stat.MaxHealth] + event.amount,
+        );
+        // Rescale
+        round.setStat(
+          Stat.Health,
+          event.source,
+          currentHealth * event.source.stats[Stat.MaxHealth],
+        );
+      }
+    });
 
-  game.on(RoundEventType.RemoveStat, StatPriority.Exact, event => {
-    if (event.type === Stat.MaxHealth) {
-      log(`${event.source.name} lost ${event.amount} of Max Health`);
-      // Get the current health percentage
-      const currentHealth =
-        event.source.stats[Stat.Health] / event.source.stats[Stat.MaxHealth];
-      game.setStat(
-        Stat.MaxHealth,
-        event.source,
-        event.source.stats[Stat.MaxHealth] - event.amount,
-      );
-      // Rescale
-      game.setStat(
-        Stat.Health,
-        event.source,
-        currentHealth * event.source.stats[Stat.MaxHealth],
-      );
-    }
+    round.on(RoundEventType.RemoveStat, StatPriority.Exact, event => {
+      if (event.type === Stat.MaxHealth) {
+        log(`${event.source.owner.name} lost ${event.amount} of Max Health`);
+        // Get the current health percentage
+        const currentHealth =
+          event.source.stats[Stat.Health] / event.source.stats[Stat.MaxHealth];
+        round.setStat(
+          Stat.MaxHealth,
+          event.source,
+          event.source.stats[Stat.MaxHealth] - event.amount,
+        );
+        // Rescale
+        round.setStat(
+          Stat.Health,
+          event.source,
+          currentHealth * event.source.stats[Stat.MaxHealth],
+        );
+      }
+    });
   });
 }
