ae3cef578e308a12dd507fd762dfec4e5ddd3f1c
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2025-01-04 09:09:41 +0800

    Update speed.ts

--- src/core/mechanics/speed.ts
+++ src/core/mechanics/speed.ts
@@ -1,35 +1,63 @@
-import { type Game, RoundEventType, Stack } from '../game';
+import type { Game } from '../game';
 import { log } from '../log';
-import { StackPriority } from '../priorities';
+import {
+  EventPriority,
+  GameEventType,
+  RoundEventType,
+  Stack,
+  StackPriority,
+} from '../types';
 
 export function setupSpeedMechanics(game: Game): void {
-  log('Setting up Speed mechanics.');
-  game.on(RoundEventType.SetStack, StackPriority.Exact, event => {
-    if (event.type === Stack.Speed) {
-      log(`${event.source.name}'s Speed changed to ${event.amount}`);
-      event.source.stacks[Stack.Speed] = event.amount;
-    }
-  });
+  game.on(GameEventType.NextRound, EventPriority.Pre, ({ round }) => {
+    log('Setting up Speed mechanics.');
 
-  game.on(RoundEventType.AddStack, StackPriority.Exact, event => {
-    if (event.type === Stack.Speed) {
-      log(`${event.source.name} gained ${event.amount} stacks of Speed`);
-      game.setStack(
-        Stack.Speed,
-        event.source,
-        event.source.stacks[Stack.Speed] + event.amount,
-      );
-    }
-  });
+    round.on(RoundEventType.SetStack, StackPriority.Exact, event => {
+      if (event.type === Stack.Speed) {
+        log(`${event.source.owner.name}'s Speed changed to ${event.amount}`);
+        event.source.stacks[Stack.Speed] = event.amount;
+      }
+    });
 
-  game.on(RoundEventType.RemoveStack, StackPriority.Exact, event => {
-    if (event.type === Stack.Speed) {
-      log(`${event.source.name} lost ${event.amount} stacks of Speed`);
-      game.setStack(
-        Stack.Speed,
-        event.source,
-        event.source.stacks[Stack.Speed] - event.amount,
-      );
-    }
+    round.on(RoundEventType.AddStack, StackPriority.Exact, event => {
+      if (event.type === Stack.Speed) {
+        /**
+         * Counter Slow by removing stacks from it
+         */
+        if (event.source.stacks[Stack.Slow] > 0) {
+          const diff = event.source.stacks[Stack.Slow] - event.amount;
+          round.removeStack(Stack.Slow, event.source, event.amount);
+
+          if (diff < 0) {
+            log(`${event.source.owner.name} gained ${-diff} stacks of Speed`);
+            round.setStack(
+              Stack.Speed,
+              event.source,
+              event.source.stacks[Stack.Speed] - diff,
+            );
+          }
+        } else {
+          log(
+            `${event.source.owner.name} gained ${event.amount} stacks of Speed`,
+          );
+          round.setStack(
+            Stack.Speed,
+            event.source,
+            event.source.stacks[Stack.Speed] + event.amount,
+          );
+        }
+      }
+    });
+
+    round.on(RoundEventType.RemoveStack, StackPriority.Exact, event => {
+      if (event.type === Stack.Speed) {
+        log(`${event.source.owner.name} lost ${event.amount} stacks of Speed`);
+        round.setStack(
+          Stack.Speed,
+          event.source,
+          event.source.stacks[Stack.Speed] - event.amount,
+        );
+      }
+    });
   });
 }
