03b88e65df66bb94dadca8b2d3a56dfe1b4c85ea
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2025-01-04 16:26:47 +0800

    Update card-406.ts

--- src/core/effects/add-stack-bonus/card-406.ts
+++ src/core/effects/add-stack-bonus/card-406.ts
@@ -1,9 +1,8 @@
-import type { CardManipulator } from '../../card';
 import { Stack } from '../../types';
 import { AddStackBonusCard } from './template';
 
 export class AddCurseStackBonusCard extends AddStackBonusCard {
-  constructor(manipulator: CardManipulator) {
-    super(manipulator, Stack.Curse);
+  constructor() {
+    super(Stack.Curse);
   }
 }
