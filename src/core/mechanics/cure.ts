e65955db074e9c9c1df64e32038161b7e4b5b130
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2025-01-04 09:14:03 +0800

    Update cure.ts

--- src/core/mechanics/cure.ts
+++ src/core/mechanics/cure.ts
@@ -1,60 +1,77 @@
-import { DamageType } from '../damage';
-import { type Game, RoundEventType, Stack } from '../game';
+import type { Game } from '../game';
 import { log } from '../log';
-import { StackPriority } from '../priorities';
+import {
+  EventPriority,
+  GameEventType,
+  RoundEventType,
+  Stack,
+  StackPriority,
+} from '../types';
 
 const CONSUMABLE_STACKS = 0.5;
 
 export function setupCureMechanics(game: Game): void {
-  log('Setting up Cure mechanics.');
-  game.on(RoundEventType.SetStack, StackPriority.Exact, event => {
-    if (event.type === Stack.Cure) {
-      log(`${event.source.name}'s Cure changed to ${event.amount}`);
-      event.source.stacks[Stack.Cure] = event.amount;
-    }
-  });
+  game.on(GameEventType.NextRound, EventPriority.Pre, ({ round }) => {
+    log('Setting up Cure mechanics.');
 
-  game.on(RoundEventType.AddStack, StackPriority.Exact, event => {
-    if (event.type === Stack.Cure) {
-      log(`${event.source.name} gained ${event.amount} stacks of Cure`);
-      game.setStack(
-        Stack.Cure,
-        event.source,
-        event.source.stacks[Stack.Cure] + event.amount,
-      );
-    }
-  });
-
-  game.on(RoundEventType.RemoveStack, StackPriority.Exact, event => {
-    if (event.type === Stack.Cure) {
-      log(`${event.source.name} lost ${event.amount} stacks of Cure`);
-      game.setStack(
-        Stack.Cure,
-        event.source,
-        event.source.stacks[Stack.Cure] - event.amount,
-      );
-    }
-  });
-
-  game.on(RoundEventType.ConsumeStack, StackPriority.Exact, event => {
-    if (event.type === Stack.Cure) {
-      const stacks = event.source.stacks[Stack.Cure];
-      if (stacks !== 0) {
-        game.removeStack(
+    round.on(RoundEventType.ConsumeStack, StackPriority.Exact, event => {
+      if (event.type === Stack.Cure) {
+        const current = event.source.stacks[Stack.Cure];
+        round.removeStack(
           Stack.Cure,
           event.source,
-          Math.abs(stacks) === 1 ? stacks : stacks * CONSUMABLE_STACKS,
+          current === 1 ? current : current * CONSUMABLE_STACKS,
         );
-        if (stacks < 0) {
-          game.dealDamage(
-            DamageType.Poison,
-            game.getOppositePlayer(event.source),
+      }
+    });
+
+    round.on(RoundEventType.SetStack, StackPriority.Exact, event => {
+      if (event.type === Stack.Cure) {
+        const clamped = Math.max(0, event.amount);
+        log(`${event.source.owner.name}'s Cure stacks changed to ${clamped}`);
+        event.source.stacks[Stack.Cure] = clamped;
+      }
+    });
+
+    round.on(RoundEventType.AddStack, StackPriority.Exact, event => {
+      if (event.type === Stack.Cure) {
+        /**
+         * Counter Poison by removing stacks from it
+         */
+        if (event.source.stacks[Stack.Poison] > 0) {
+          const diff = event.source.stacks[Stack.Poison] - event.amount;
+          round.removeStack(Stack.Poison, event.source, event.amount);
+
+          if (diff < 0) {
+            log(`${event.source.owner.name} gained ${-diff} stacks of Cure`);
+            round.setStack(
+              Stack.Cure,
+              event.source,
+              event.source.stacks[Stack.Cure] - diff,
+            );
+          }
+        } else {
+          log(
+            `${event.source.owner.name} gained ${event.amount} stacks of Cure`,
+          );
+          round.setStack(
+            Stack.Cure,
             event.source,
-            -stacks,
-            0,
+            event.source.stacks[Stack.Cure] + event.amount,
           );
         }
       }
-    }
+    });
+
+    round.on(RoundEventType.RemoveStack, StackPriority.Exact, event => {
+      if (event.type === Stack.Cure) {
+        log(`${event.source.owner.name} lost ${event.amount} stacks of Cure`);
+        round.setStack(
+          Stack.Cure,
+          event.source,
+          event.source.stacks[Stack.Cure] - event.amount,
+        );
+      }
+    });
   });
 }
