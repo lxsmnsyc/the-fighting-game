8376ad3934b0b69c1bba475d02931de6f38cf72f
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2025-01-11 01:06:35 +0800

    Update player.ts

--- src/core/player.ts
+++ src/core/player.ts
@@ -1,22 +1,18 @@
 import { AleaRNG } from './alea';
 import type { Card, CardInstance } from './card';
-import { DEFAULT_ATTACK, DEFAULT_MAGIC, DEFAULT_MAX_HEALTH } from './constants';
+import { DEFAULT_MAX_HEALTH } from './constants';
 import type { Unit } from './round';
 import { Print, type PrintSpawnChance, Stat } from './types';
 
 export interface PlayerStats {
   [Stat.MaxHealth]: number;
   [Stat.Health]: number;
-  [Stat.Attack]: number;
-  [Stat.Magic]: number;
 }
 
 export class Player {
   stats: PlayerStats = {
     [Stat.MaxHealth]: DEFAULT_MAX_HEALTH,
     [Stat.Health]: DEFAULT_MAX_HEALTH,
-    [Stat.Attack]: DEFAULT_ATTACK,
-    [Stat.Magic]: DEFAULT_MAGIC,
   };
 
   printSpawnChance: PrintSpawnChance = {
