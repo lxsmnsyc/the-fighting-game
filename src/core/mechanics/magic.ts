8284dc3e4d60a2bb5400b7868947e28f1f919414
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2025-01-11 01:06:31 +0800

    Update magic.ts

--- src/core/mechanics/magic.ts
+++ src/core/mechanics/magic.ts
@@ -1,41 +1,68 @@
 import type { Game } from '../game';
 import { log } from '../log';
 import {
+  DamageType,
   EventPriority,
   GameEventType,
   RoundEventType,
-  Stat,
-  StatPriority,
+  Stack,
+  StackPriority,
 } from '../types';
+
+const CONSUMABLE_STACKS = 0.4;
 
 export function setupMagicMechanics(game: Game): void {
   game.on(GameEventType.NextRound, EventPriority.Pre, ({ round }) => {
     log('Setting up Magic mechanics.');
-    round.on(RoundEventType.SetStat, StatPriority.Exact, event => {
-      if (event.type === Stat.Magic) {
-        log(`${event.source.owner.name}'s Magic changed to ${event.amount}`);
-        event.source.stats[Stat.Magic] = Math.max(0, event.amount);
-      }
-    });
 
-    round.on(RoundEventType.AddStat, StatPriority.Exact, event => {
-      if (event.type === Stat.Magic) {
-        log(`${event.source.owner.name} gained ${event.amount} of Magic`);
-        round.setStat(
-          Stat.Magic,
+    round.on(RoundEventType.ConsumeStack, StackPriority.Exact, event => {
+      if (event.type === Stack.Magic) {
+        const current = event.source.stacks[Stack.Magic];
+        if (current > 0) {
+          round.dealDamage(
+            DamageType.Magic,
+            event.source,
+            round.getEnemyUnit(event.source),
+            current,
+            0,
+          );
+        }
+        round.removeStack(
+          Stack.Magic,
           event.source,
-          event.source.stats[Stat.Magic] + event.amount,
+          current === 1 ? current : current * CONSUMABLE_STACKS,
         );
       }
     });
 
-    round.on(RoundEventType.RemoveStat, StatPriority.Exact, event => {
-      if (event.type === Stat.Magic) {
-        log(`${event.source.owner.name} lost ${event.amount} of Magic`);
-        round.setStat(
-          Stat.Magic,
+    round.on(RoundEventType.SetStack, StackPriority.Exact, event => {
+      if (event.type === Stack.Magic) {
+        const clamped = Math.max(0, event.amount);
+        log(`${event.source.owner.name}'s Magic stacks changed to ${clamped}`);
+        event.source.stacks[Stack.Magic] = clamped;
+      }
+    });
+
+    round.on(RoundEventType.AddStack, StackPriority.Exact, event => {
+      if (event.type === Stack.Magic) {
+        log(
+          `${event.source.owner.name} gained ${event.amount} stacks of Magic`,
+        );
+        round.setStack(
+          Stack.Magic,
           event.source,
-          event.source.stats[Stat.Magic] - event.amount,
+          event.source.stacks[Stack.Magic] + event.amount,
+        );
+      }
+    });
+
+    round.on(RoundEventType.RemoveStack, StackPriority.Exact, event => {
+      if (event.type === Stack.Magic) {
+        log(`${event.source.owner.name} lost ${event.amount} stacks of Magic`);
+        round.setStack(
+          Stack.Magic,
+          event.source,
+          event.source.stacks[Stack.Magic] - event.amount,
         );
       }
     });
