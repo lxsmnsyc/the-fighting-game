5512c4ce2c540ab58739cbbdff7b55db1fbe0f61
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2025-01-11 10:08:56 +0800

    Update index.ts

--- src/core/cards/common/index.ts
+++ src/core/cards/common/index.ts
@@ -1,4 +1,7 @@
-import addStackBonus from './add-stack-bonus';
-import addStackOnStart from './add-stack-on-start';
+import { ADD_STACK_BONUS_CARDS } from './add-stack-bonus';
+import { ADD_STACK_ON_START_CARDS } from './add-stack-on-start';
 
-export default [...addStackBonus, ...addStackOnStart];
+export const COMMON_CARDS = [
+  ...ADD_STACK_BONUS_CARDS,
+  ...ADD_STACK_ON_START_CARDS,
+];
